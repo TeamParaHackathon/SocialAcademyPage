aa7113fb6377d621cb7600323a3a4c93aead25e7
Kevin Garcia <gkevinyamil@gmail.com>
2021-03-13 00:11:34 -0600

    Comppniendo la seccion de cursos. Todavia no esta completa

---
+++ Js/Forum.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	var typed = new Typed(".typing", {
+		strings: [
+			"Informatica General",
+			"Diseño grafico",
+			"Desarrollo Web",
+			"Marketing",
+            "Desarrollo Movil",
+			"Contabilidad",
+			"Progamación General",
+		],
+		typeSpeed: 150,
+		backSpeed: 70,
+		loop: true,
+	});
+});
