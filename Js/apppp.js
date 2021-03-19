ca0b6e2951c506f3d9376a057586d74780f4428c
Andres179012 <71681723+Andres179012@users.noreply.github.com>
2021-03-19 13:49:45 -0600

    Sub_Categorias
    Se cambio de documentacion a Sub_Categoria dado que es donde se hara un filtrado de sub_categorias, asi como ligeros cambios en el diseño (No planeo dejarlos asi, los voy a mejorar), pero  la idea general es esa

--- Js/apppp.js
+++ Js/apppp.js
@@ -21,3 +21,56 @@ let buscador = new Search({
         'Edicion de video en Filmora'
     ]
 });
+
+
+
+///readmore//
+
+/*
+let button = document.querySelector('#button').addEventListener('click', function() {
+    
+    if(!i){
+        document.getElementById('readmore').style.display = 'flex';
+        document.getElementById('button').innerHTML = 'Read Less';
+        i = 1;
+    }
+    else{
+        document.getElementById('readmore').style.display = 'none';
+        document.getElementById('button').innerHTML = 'Read More';
+        i = 0;
+    }
+})*/
+let i = 0;
+jQuery(document).on('click', 'div.botton #button',
+    function (event){
+        if(!i){
+            event.preventDefault();
+            jQuery('.contenidooculto').slideToggle();
+            document.getElementById('button').innerHTML = 'Ver menos..';
+            i = 1;
+        }
+        else{
+            event.preventDefault();
+            jQuery('.contenidooculto').slideToggle();
+            document.getElementById('button').innerHTML = 'Ver más..';
+            i = 0;
+        }
+    });
+
+
+/* Navegacion con tabs*/
+$(document).ready(function(){
+    $('ul.tabs li a:first').addClass('active');
+    $('.secciones article').hide();
+    $('.secciones article:first').show();
+
+    $('ul.tabs li a').click(function(){
+        $('ul.tabs li a').removeClass('active');
+        $(this).addClass('active');
+        $('.secciones article').hide();
+
+        var activeTab = $(this).attr('href');
+        $(activeTab).show();
+        return false;
+    });
+})
