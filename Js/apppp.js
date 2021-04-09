673c34bb7f3fd3602c4e948035ed1ea46080cee2
Andres179012 <71681723+Andres179012@users.noreply.github.com>
2021-04-09 10:38:30 -0600

    Cambios en la logica

--- Js/apppp.js
+++ Js/apppp.js
@@ -22,55 +22,21 @@ let buscador = new Search({
     ]
 });
 
+/* Tabs de Documentos, Videos y proyectos */
 
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
 
-///readmore//
-
-/*
-let button = document.querySelector('#button').addEventListener('click', function() {
-    
-    if(!i){
-        document.getElementById('readmore').style.display = 'flex';
-        document.getElementById('button').innerHTML = 'Read Less';
-        i = 1;
-    }
-    else{
-        document.getElementById('readmore').style.display = 'none';
-        document.getElementById('button').innerHTML = 'Read More';
-        i = 0;
-    }
-})*/
-let i = 0;
-jQuery(document).on('click', 'div.botton #button',
-    function (event){
-        if(!i){
-            event.preventDefault();
-            jQuery('.contenidooculto').slideToggle();
-            document.getElementById('button').innerHTML = '---------Ver menos----------';
-            i = 1;
-        }
-        else{
-            event.preventDefault();
-            jQuery('.contenidooculto').slideToggle();
-            document.getElementById('button').innerHTML = '----------Ver más-----------';
-            i = 0;
-        }
-    });
-
-
-/* Navegacion con tabs*/
-$(document).ready(function(){
-    $('ul.tabs li a:first').addClass('active');
-    $('.secciones article').hide();
-    $('.secciones article:first').show();
-
-    $('ul.tabs li a').click(function(){
-        $('ul.tabs li a').removeClass('active');
-        $(this).addClass('active');
-        $('.secciones article').hide();
-
-        var activeTab = $(this).attr('href');
-        $(activeTab).show();
-        return false;
-    });
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
 })
